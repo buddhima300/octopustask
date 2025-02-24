21847e967903b453cd4703bdb2fee607700c20a3
buddhima chathuranga <buddhimachathuranga300@gmail.com>
2025-02-24 21:18:05 +0530

    complete the SideBar component

--- octopus/src/App.js
+++ octopus/src/App.js
@@ -1,11 +1,18 @@
-import "./App.css";
+import { Box } from "@chakra-ui/react";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
-    <div className="App">
+    <Box
+      bgBlendMode="hard-light"
+      // bgGradient="to-r"
+      // gradientFrom="purple.800"
+      // gradientTo="purple.500"
+      backgroundImage="url(https://4kwallpapers.com/images/wallpapers/outrun-neon-dark-background-purple-3440x1440-4523.jpg)"
+      bgSize="cover"
+    >
       <Dashboard />
-    </div>
+    </Box>
   );
 }
 
---
+++ octopus/src/components/Dashcomponents/SideBar.jsx
@@ -0,0 +1,42 @@
+import { Box, Link, Text, VStack } from "@chakra-ui/react";
+import React from "react";
+
+function SideBar({ menu }) {
+  return (
+    <div>
+      <Box
+        width={400}
+        height="100vh"
+        borderRadius="md"
+        boxShadow="md"
+        backdropFilter={"blur(15px)"}
+      >
+        <Text textStyle="2xl" color="white" p={5}>
+          ANALYTICS
+        </Text>
+        {/* menu */}
+        <VStack spacing={5} align="start" p={5}>
+          {menu.map((item) => (
+            <Link
+              href={item.href}
+              bg="rgb(255, 255, 255,0.1)"
+              p={4}
+              width="100%"
+              backdropFilter="blur(25px)"
+              borderRadius={10}
+              _hover={{ bg: "rgb(255, 255, 255,0.2)" }}
+              mt={5}
+            >
+              {item.icon}
+              <Text fontWeight="bold" color="white">
+                {item.name}
+              </Text>
+            </Link>
+          ))}
+        </VStack>
+      </Box>
+    </div>
+  );
+}
+
+export default SideBar;
